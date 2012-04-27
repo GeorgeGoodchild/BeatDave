41dcf74b43061d3bd0196ee21c8982ae69138976
George Goodchild <GeorgeGoodchild@GMail.com>
2012-04-27 17:58:07 +0200

    Update BeatDave.Web/Areas/Api_v1/ViewModels/DataSetInput.cs

--- BeatDave.Web/Areas/Api_v1/ViewModels/DataSetInput.cs
+++ BeatDave.Web/Areas/Api_v1/ViewModels/DataSetInput.cs
@@ -1,16 +1,39 @@
-﻿using System.Collections.Generic;
+using System;
+using System.Collections.Generic;
+using BeatDave.Web.Models;
 
-namespace BeatDave.Web.Areas.Api_v1.ViewModels
+namespace BeatDave.Web.Areas.Api_v1.Contracts
 {
-    public class DataSetInput
+    public class DataPointContract
+    {
+        public int Id { get; set; }
+        public double Value { get; set; }
+        public DateTime OccurredOn { get; set; }
+    }
+    
+    public class DataSetContract
     {
         public string Title { get; set; }
         public string Description { get; set; }
-        public Units Units { get; set; }
-        public List<DataPoint> DataPoints { get; set; }
-        public List<string> Tags { get; set; }
-        public List<ISocialNetworkAccount> AutoShareOn { get; set; }
+        public UnitsContract Units { get; set; }
         public string OwnerId { get; set; }
         public Visibility Visibility { get; set; }
+    }
+    
+    public class DenormalizedReference
+    {
+        public string Id { get; set; }
+        public string DisplayName { get; set; }
+    }
+    
+    public class UnitsContract
+    {
+        public string Symbol { get; set; }
+        public SymbolPosition SymbolPosition { get; set; }
+        public int Precision { get; set; }
+    }
+    
+    public class UserContract
+    {
     }
 }

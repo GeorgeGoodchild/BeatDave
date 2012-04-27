dc7059c31290b50c4e01337f2a39cc5ee7d7dd16
George Goodchild <GeorgeGoodchild@GMail.com>
2012-04-27 17:52:12 +0200

    Update BeatDave.Web/Models/DataSet.cs

--- BeatDave.Web/Models/DataSet.cs
+++ BeatDave.Web/Models/DataSet.cs
@@ -19,11 +19,13 @@ namespace BeatDave.Web.Models
         public string Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
+        public List<string> Tags { get; set; }
+        
         public Units Units { get; set; }
         public List<DataPoint> DataPoints { get; set; }
-        public List<string> Tags { get; set; }
-        public List<ISocialNetworkAccount> AutoShareOn { get; set; }
+        
         public string OwnerId { get; set; }
+        public List<ISocialNetworkAccount> AutoShareOn { get; set; }        
         public Visibility Visibility { get; set; }
     }
 }

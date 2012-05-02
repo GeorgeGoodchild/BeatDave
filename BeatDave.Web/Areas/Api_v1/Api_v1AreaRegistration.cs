0c7e859d525fdfb68d84adb1decf64ba698ca8e3
George Goodchild <GeorgeGoodchild@GMail.com>
2012-05-02 18:42:29 +0200

    Update BeatDave.Web/Areas/Api_v1/Api_v1AreaRegistration.cs

--- BeatDave.Web/Areas/Api_v1/Api_v1AreaRegistration.cs
+++ BeatDave.Web/Areas/Api_v1/Api_v1AreaRegistration.cs
@@ -1,4 +1,4 @@
-﻿using System.Web.Http;
+using System.Web.Http;
 using System.Web.Mvc;
 
 namespace BeatDave.Web.Areas.Api_v1
@@ -9,9 +9,12 @@ namespace BeatDave.Web.Areas.Api_v1
 
         public override void RegisterArea(AreaRegistrationContext context)
         {
-            context.Routes.MapHttpRoute(name: "Api_v1_Default",
-                                        routeTemplate: "api/v1/{controller}/{*id}",
-                                        defaults: new { id = RouteParameter.Optional });
+            //context.Routes.MapHttpRoute(name: "Api_v1_Default",
+            //                            routeTemplate: "api/v1/{controller}/{id}",
+            //                            defaults: new { id = RouteParameter.Optional });
+
+            context.Routes.MapHttpRoute(name: "Api_v1_DataSets",    routeTemplate: "api/v1/DataSets/{dataSetId}",                           defaults: new { controller = "DataSets", dataSetId = RouteParameter.Optional });
+            context.Routes.MapHttpRoute(name: "Api_v1_DataPoints",  routeTemplate: "api/v1/DataSets/{dataSetId}/DataPoints/{dataPointId}",  defaults: new { controller = "DataPoints", dataPointId = RouteParameter.Optional });
         }
     }
 }

5578ed9b1e66319700463e8fdea7c8790d22918d
George Goodchild <GeorgeGoodchild@GMail.com>
2012-05-03 18:42:57 +0200

    Update BeatDave.Web/Areas/Api_v1/Controllers/DataSetsController.cs

--- BeatDave.Web/Areas/Api_v1/Controllers/DataSetsController.cs
+++ BeatDave.Web/Areas/Api_v1/Controllers/DataSetsController.cs
@@ -5,12 +5,14 @@ using BeatDave.Web.Areas.Api_v1.Models;
 using BeatDave.Web.Infrastructure;
 using BeatDave.Web.Models;
 using Raven.Client.Linq;
+using System.Web.Http;
 
 namespace BeatDave.Web.Areas.Api_v1.Controllers
 {
     public class DataSetsController : FatApiController
     {
         // GET /Api/v1/DataSets
+        [Authorize]
         public HttpResponseMessage<List<DataSetView>> Get(string q = "", int skip = DefaultSkip, int take = DefaultTake)
         {
             var dataSets = base.RavenSession.Query<DataSet>()
@@ -25,6 +27,7 @@ namespace BeatDave.Web.Areas.Api_v1.Controllers
         }
 
         // GET /Api/v1/DataSets/33
+        [Authorize]
         public HttpResponseMessage<DataSetView> Get(int dataSetId)
         {
             if (dataSetId <= 0)
@@ -40,9 +43,10 @@ namespace BeatDave.Web.Areas.Api_v1.Controllers
             return Ok(dataSetView);
         }
 
-       
+
 
         // POST /Api/v1/DataSets
+        [Authorize]
         public HttpResponseMessage<DataSetView> Post(DataSetInput dataSetInput)
         {
             if (ModelState.IsValid == false)
@@ -60,26 +64,32 @@ namespace BeatDave.Web.Areas.Api_v1.Controllers
 
 
 
-        // PUT /Api/v1/DataSets
-        public HttpResponseMessage Put(DataSetInput dataSetInput)
+        // PUT /Api/v1/DataSets/33
+        [Authorize]
+        public HttpResponseMessage Put([FromUri]int? dataSetId, DataSetInput dataSetInput)
         {
-            if (dataSetInput.IsNewDataSet())
-                ModelState.AddModelError("Id", "DataSet Id is missing");
+            // HACK: Once out of beta the dataSetId parameter should be bound from the Url rather than the request body
+            //       Stop the parameter being nullable too
+            if (dataSetId == null) dataSetId = dataSetInput.DataSetId;
+
+            if (dataSetId <= 0)
+                return BadRequest<DataSetView.DataPointView>(null, "Data Set Id is missing");
 
             if (ModelState.IsValid == false)
                 return BadRequest(ModelState.FirstErrorMessage());
 
-            var dataSet = base.RavenSession.Load<DataSet>(dataSetInput.Id);
+            var dataSet = base.RavenSession.Load<DataSet>(dataSetId);
             dataSetInput.MapToInstance(dataSet);
            
             var dataSetView = dataSet.MapTo<DataSetView>();
 
             return Ok(dataSetView);
         }
-        
+
 
 
         // DELETE /Api/v1/DataSets/5
+        [Authorize]
         public HttpResponseMessage Delete(int dataSetId)
         {
             if (dataSetId <= 0)

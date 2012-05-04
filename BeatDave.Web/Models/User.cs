597c413bcefca5b57765d2fefb91da3c36165e0f
George Goodchild <GeorgeGoodchild@GMail.com>
2012-05-04 16:55:11 +0200

    Update BeatDave.Web/Models/User.cs

--- BeatDave.Web/Models/User.cs
+++ BeatDave.Web/Models/User.cs
@@ -1,16 +1,15 @@
-﻿
+
 namespace BeatDave.Web.Models
 {
     using System.Collections.Generic;
 
     public class User
     {
-        public string Id { get; set; }
+        public string Username { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
-        public string EmailVerificationCode { get; set; }
-        public bool EmailVerified { get; set; }
+        public string AspNetId { get; set; }
         public List<ISocialNetworkAccount> SocialNetworkAccounts { get; set; }
         public List<Friend> Friends { get; set; }
     }

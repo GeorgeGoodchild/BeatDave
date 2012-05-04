0a0f4b908a622a346bb5ee9d19dade4a9cae8b23
George Goodchild <GeorgeGoodchild@GMail.com>
2012-05-04 16:55:27 +0200

    Update BeatDave.Web/Models/Friend.cs

--- BeatDave.Web/Models/Friend.cs
+++ BeatDave.Web/Models/Friend.cs
@@ -1,9 +1,9 @@
-﻿
+
 namespace BeatDave.Web.Models
 {
     public class Friend
     {
-        public string FriendId { get; set; }
+        public string FriendUsername { get; set; }
         public bool Confirmed { get; set; }
     }
 }
